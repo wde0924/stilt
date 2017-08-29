23db7b9ff504e4a470b41af4d62aef4d542bc15c
benfasoli <benfasoli@gmail.com>
2017-08-28 20:25:56 -0600

    Transparent nav on front page
    Former-commit-id: 100d7a0c1e6c889cca16ac4773835624d4544575

--- _site/js/particle-background.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  Particles.init({
-    selector: '.bg',
-    maxParticles: 300,
-    color: '#6a7191',
-    sizeVariations: 2,
-    speed: 1,
-  });
-};
--- _site/js/tutorials.js
+++
@@ -1,7 +0,0 @@
-function show(id) {
-  var elements = document.getElementsByClassName('showhide');
-  for (var i = 0; i < elements.length; i++)
-      elements[i].style.display = 'none';
-
-  document.getElementById(id).style.display='block';
-}
