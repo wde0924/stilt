010e6ef83c25d5a2624be3dd5b6771dcd107b02f
Ben Fasoli <benfasoli@gmail.com>
2017-12-31 11:31:17 -0700

    Updated docs link
    Former-commit-id: 78a12e433b6a4e5a676452cdc7f8269b08ce286e

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,6 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
-
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
     return gulp.src('less/new-age.less')
