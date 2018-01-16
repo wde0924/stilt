0248a074e3a8e3c130081f558d54eb525b88eff4
Ben Fasoli <benfasoli@gmail.com>
2018-01-16 13:48:57 -0700

    Change particle color and speed
    Former-commit-id: 04e101efc278d95fdc51a0ac63f67760a3d3b724

--- js/particle-background.js
+++ js/particle-background.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   Particles.init({
     selector: '.bg',
-    maxParticles: 300,
-    color: '#6a7191',
-    sizeVariations: 2,
-    speed: 1,
+    maxParticles: 400,
+    color: '#40455A',
+    sizeVariations: 3,
+    speed: 1.2
   });
 };
